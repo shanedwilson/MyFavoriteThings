888d5e43334157555bee7a87c8e245df8629883c
Shane D Wilson <me@shanedwilson.com>
2019-03-02 21:31:55 -0600

    Add project files.

---
+++ MyFavoriteThings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFavoriteThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
