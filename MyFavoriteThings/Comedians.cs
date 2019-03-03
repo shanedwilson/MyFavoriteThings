d00833659a8b56f9abd613c1909b5c15ffee208b
Shane D Wilson <me@shanedwilson.com>
2019-03-03 17:27:28 -0600

    added more favorites

---
+++ MyFavoriteThings/Comedians.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFavoriteThings
+{
+    class Comedians
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string BestWork { get; }
+
+        public Comedians(string firstname, string lastname, string bestwork)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            BestWork = bestwork;
+        }
+
+    }
+}
---
+++ MyFavoriteThings/Whiskey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFavoriteThings
+{
+    class Whiskey
+    {
+        public string Brand { get; }
+        public string Name { get; }
+        public string Proof { get; }
+
+        public Whiskey(string brand, string name, string proof)
+        {
+            Brand = brand;
+            Name = name;
+            Proof = proof;
+        }
+    }
+}
